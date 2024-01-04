5594ac837f5ed127fce61e519dad5c04741f6dbb
Kosya15 <hrumak15@gmail.com>
2024-01-04 15:00:44 +0200

    Fixed config.py #7

--- project/config.py
+++ project/config.py
@@ -3,12 +3,9 @@ import os
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
 
-db_user = os.environ['DB_USER']
-db_password = os.environ['DB_PASSWORD']
-db_name = os.environ['DB_NAME']
-db_host = os.environ['DB_HOST']
-SQLALCHEMY_DATABASE_URI = f"postgres://lab3db_o1ps_user:sEXzofH6ojrO5jp3p47KtjIegDedDWTQ@dpg-cmb8hued3nmc73eo9aqg-a.oregon-postgres.render.com/lab3db_o1ps"             
-
-#"postgresql://lab3db_o1ps_user:sEXzofH6ojrO5jp3p47KtjIegDedDWTQ@dpg-cmb8hued3nmc73eo9aqg-a/lab3db_o1ps"
-# postgres://lab3db_o1ps_user:sEXzofH6ojrO5jp3p47KtjIegDedDWTQ@dpg-cmb8hued3nmc73eo9aqg-a/lab3db_o1ps 
+#db_user = os.environ['DB_USER']
+#db_password = os.environ['DB_PASSWORD']
+#db_name = os.environ['DB_NAME']
+#db_host = os.environ['DB_HOST']
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")             
 SQLALCHEMY_TRACK_MODIFICATIONS = False
