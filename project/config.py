20663ec81cbd453ea886ebf12b73c66b971652f9
Kosya15 <hrumak15@gmail.com>
2024-01-04 14:03:01 +0200

    Fixing config.py #4

--- project/config.py
+++ project/config.py
@@ -7,5 +7,6 @@ FLASK_DEBUG = True
 # db_password = os.environ['DB_PASSWORD']
 # db_name = os.environ['DB_NAME']
 # db_host = os.environ['DB_HOST']
-SQLALCHEMY_DATABASE_URI = "postgres://lab3db_o1ps_user:sEXzofH6ojrO5jp3p47KtjIegDedDWTQ@dpg-cmb8hued3nmc73eo9aqg-a.oregon-postgres.render.com/lab3db_o1ps"
+SQLALCHEMY_DATABASE_URI = "postgresql://lab3db_o1ps_user:sEXzofH6ojrO5jp3p47KtjIegDedDWTQ@dpg-cmb8hued3nmc73eo9aqg-a/lab3db_o1ps"
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
