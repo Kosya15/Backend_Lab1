7c6f1b77c182ee95f4f99366e148e92645e840ab
Kosya15 <hrumak15@gmail.com>
2024-01-04 12:55:27 +0200

    Fixing config.py for deploying

--- project/config.py
+++ project/config.py
@@ -3,10 +3,9 @@ import os
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
 
-db_user = os.environ.get('DB_USER', 'post')
-db_password = os.environ.get('DB_PASSWORD', 'son1540')
-db_name = os.environ.get('DB_NAME', 'lab3DB')
-db_host = os.environ.get('DB_HOST', 'localhost')
-
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+db_name = os.environ['DB_NAME']
+db_host = os.environ['DB_HOST']
 SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
