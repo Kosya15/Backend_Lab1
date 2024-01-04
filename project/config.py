3e2b9290846ec344b9d63703aa51415bb8949ced
Kosya15 <hrumak15@gmail.com>
2024-01-04 13:24:10 +0200

    Fixing config.py #2

--- project/config.py
+++ project/config.py
@@ -3,9 +3,9 @@ import os
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
 
-db_user = os.environ['DB_USER']
-db_password = os.environ['DB_PASSWORD']
-db_name = os.environ['DB_NAME']
-db_host = os.environ['DB_HOST']
-SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
+# db_user = os.environ['DB_USER']
+# db_password = os.environ['DB_PASSWORD']
+# db_name = os.environ['DB_NAME']
+# db_host = os.environ['DB_HOST']
+SQLALCHEMY_DATABASE_URI = "postgres://lab3db_o1ps_user:sEXzofH6ojrO5jp3p47KtjIegDedDWTQ@dpg-cmb8hued3nmc73eo9aqg-a/lab3db_o1ps"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
